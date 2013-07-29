def1e320d59e1c0f8b198d4995e33dcfae958455
Damola Mabogunje <damola@mabogunje.net>
2013-07-29 07:55:44 -0400

    Code cleanup Now using immutables throughout; Simplified some functions; Eliminated unnecessary classes; Did some renaming

--- src/grammar.scala
+++ src/grammar.scala
@@ -1,6 +1,4 @@
 import java.util.Locale
-import collection.mutable._
-
 import YorubaImplicits._
 
 /**
@@ -16,12 +14,12 @@ import Bias._
 /**
  * Word properties are tokens for word features (helps parsing)
  */
-abstract class WordProperty { 
+trait WordProperty { 
   def bias = Bias.None
 }
 case object Root extends WordProperty
-case class Elided(where:Bias) extends WordProperty { override def bias = where}
-case class Assimilated(where:Bias) extends WordProperty { override def bias = where}
+case class Elided(override val bias:Bias) extends WordProperty
+case class Assimilated(override val bias:Bias) extends WordProperty
 
 /**
  * Yoruba contract. All Yoruba word objects must implement these features
@@ -30,79 +28,54 @@ sealed trait Yoruba {
   def spelling:String
   def properties:Seq[WordProperty]
   def root: Yoruba
-  def isRoot: Boolean
-  def isElided:Boolean
-  def isAssimilated:Boolean
-  def toYoruba:String
-  def as(that:WordProperty):Yoruba
 
-  override def equals(o:Any) = o match {
-    case that:Yoruba => that.toYoruba.equalsIgnoreCase(this.toYoruba)
-    case that:String => that.equalsIgnoreCase(this.toYoruba)
-    case _ => super.equals(o)
-  }
+  def isRoot: Boolean = (properties contains Root)
+  def isElided:Boolean = (properties contains Elided)
+  def isAssimilated:Boolean = (properties contains Assimilated)
   
-  def hashcode = this.toYoruba.hashCode 
-  override def toString() = toYoruba
-}
-
-abstract class Expression extends Yoruba {
-  var spelling:String = ""
-  var properties:Seq[WordProperty] = Queue[WordProperty]()
-  
-  def isRoot = (properties contains Root)
-  def isElided = (properties contains Elided)
-  def isAssimilated = (properties contains Assimilated)
-  
-  def processProperties(word:String=spelling, props:List[WordProperty]=Nil):String = {
+  def abbreviation(word:String=spelling, props:List[WordProperty]=properties.toList):String = {
     props match {
       case (p:Elided) :: tail => { 
-        if (p.bias == Bias.Left) { processProperties(word.drop(1), tail) }
-        else if (p.bias == Bias.Right) { processProperties(word.dropRight(1), tail) }
-        else { processProperties(word, tail) }
+        if (p.bias == Bias.Left) { abbreviation(word.drop(1), tail) }
+        else if (p.bias == Bias.Right) { abbreviation(word.dropRight(1), tail) }
+        else { abbreviation(word, tail) }
       }
       case (p:Assimilated) :: tail => {
-        if (p.bias == Bias.Left) { processProperties(word.take(1) + word, tail) }
-        else if (p.bias == Bias.Right) { processProperties(word + word.takeRight(1), tail) }
-        else { processProperties(word, tail) }
+        if (p.bias == Bias.Left) { abbreviation(word.take(1) + word, tail) }
+        else if (p.bias == Bias.Right) { abbreviation(word + word.takeRight(1), tail) }
+        else { abbreviation(word, tail) }
       }
       case _ => word
     }
   }
   
-  def as(that:WordProperty):Yoruba = {
-    this.properties = this.properties.to[Queue] += that
-    this
-  }
-  
+  def toYoruba:String
+  def as(that:WordProperty):Yoruba 
+  override def toString = toYoruba
 }
 
 /**
  * Term is used for simple yoruba words which cannot be decomposed  
  *
  */
-case class Term(word:String, props:WordProperty*) extends Expression {
-  spelling = word
-  properties ++= props
-  
+case class Term(override val spelling:String, override val properties:Seq[WordProperty]=List()) extends Yoruba {  
   def root = this
   def toYoruba = spelling
+  
+  def as(that:WordProperty):Yoruba = this.copy(properties = this.properties :+ that)
 }
 
 /**
  * Word is used for compound yoruba words made up of Terms or other compound Words
  */
-case class Word(word:String, decomposition:List[Yoruba], props:WordProperty*) extends Expression {  
-  spelling = word
-  properties ++= props
-  
-  def root = decomposition.find(_.isRoot == true).getOrElse(this)
+case class Word(override val spelling:String, decomposition:Seq[Yoruba], override val properties:Seq[WordProperty]=List()) extends Yoruba {    
+  def root = decomposition.find(_.isRoot).getOrElse(this)
   override def isElided =  decomposition.exists(_.isElided)  || super.isElided
   override def isAssimilated = decomposition.exists(p => p.isAssimilated) || super.isAssimilated
   
-  def toYoruba:String = decomposition map { 
-    term => { processProperties(term.toYoruba, term.properties.toList) }
-  } mkString ""
+  def toYoruba:String = decomposition map { w => { w.abbreviation() }} mkString ""
+  
+  def as(that:WordProperty):Yoruba = this.copy(properties = this.properties :+ that)
 }
 
 /**
@@ -118,19 +91,14 @@ sealed trait Meaning {
 /**
  * A Yoruba translation i.e meaning in another language
  */
-case class Translation(sense:String, lang:String="en") extends Meaning {
-  var description = sense
-  var language = new Locale(lang)  
+case class Translation(override val description:String, lang:String="en") extends Meaning {
+  val language = new Locale(lang)  
 }
 
 /**
  * Convenience type for pairing a word with its attributes (used in dictionary)
  */
-case class WordEntry(word:Word, attr:Tuple2[String,String]*) {
-  var attributes = Map[String, String]() ++ attr
-  
-  def addAttributes(attrs:Tuple2[String,String]*) = for (a @ (k,v) <- attrs) {attributes += a}   
-}
+case class WordEntry(word:Word, attributes:Map[String,String])
 
 object YorubaImplicits {
   implicit def string2yoruba(str:String):Yoruba = Term(str)
@@ -139,13 +107,11 @@ object YorubaImplicits {
 object GrammarTest {
   def main(args:Array[String]) {    
     val word1 = "dé"
-    val word2 = Word("adé", List("a", Term(word1, Root)))
-    val word3 = Word("adé", List("a", word1 as Root))
-    val word4 = Word("sade", List("ṣé" as Elided(Right), word3 as Root))
+    val word2 = Word("adé", List("a", word1 as Root))
+    val word3 = Word("sade", List("ṣé" as Elided(Right), word2 as Root))
         
-    println(word1.root)
-    println(word2.root)
-    println(word3.root)
-    println(word4.root)
+    println(word1)
+    println(word2)
+    println(word3)
   }
 }
--- src/parsers.scala
+++ src/parsers.scala
@@ -11,23 +11,23 @@ class GrammarParser extends RegexParsers {
    */
   
   // Base token parsers - indicating word properties
-  def root:Parser[List[WordProperty]] = """\*""".r ^^^ {List(Root)}
+  def root:Parser[Seq[WordProperty]] = """\*""".r ^^^ {List(Root)}
   
-  def asmL:Parser[List[Assimilated]] = """<\+""".r ^^^ { List(Assimilated(Left)) }
-  def asmR:Parser[List[Assimilated]] = """\+>""".r ^^^ { List(Assimilated(Right)) }
-  def assimilation:Parser[List[Assimilated]] = asmL | asmR
+  def asmL:Parser[Seq[Assimilated]] = """<\+""".r ^^^ { List(Assimilated(Left)) }
+  def asmR:Parser[Seq[Assimilated]] = """\+>""".r ^^^ { List(Assimilated(Right)) }
+  def assimilation:Parser[Seq[Assimilated]] = asmL | asmR
   
-  def elsL:Parser[List[Elided]] = """<\-+""".r ^^ { str => 
+  def elsL:Parser[Seq[Elided]] = """<\-+""".r ^^ { str => 
     for (c <- str.tail.toList) yield Elided(Left) 
   }
-  def elsR:Parser[List[Elided]] = """\-+>""".r ^^ {str => 
+  def elsR:Parser[Seq[Elided]] = """\-+>""".r ^^ {str => 
     for (c <- str.dropRight(1).toList) yield Elided(Right) 
   }
-  def elision:Parser[List[Elided]] = elsL | elsR
+  def elision:Parser[Seq[Elided]] = elsL | elsR
 
-  def prefixes:Parser[List[WordProperty]] = asmL|elsL
-  def postfixes:Parser[List[WordProperty]] = asmR|elsR|root  
-  def property:Parser[List[WordProperty]] = root|assimilation|elision 
+  def prefixes:Parser[Seq[WordProperty]] = asmL|elsL
+  def postfixes:Parser[Seq[WordProperty]] = asmR|elsR|root  
+  def property:Parser[Seq[WordProperty]] = root|assimilation|elision 
 
   // Base parsers for all values - applies restrictions on acceptable strings
   def term:Parser[String] = """\p{L}+""".r ^^ {_.toLowerCase()}
@@ -37,7 +37,7 @@ class GrammarParser extends RegexParsers {
   def sense:Parser[String] = "/" ~ rep(value) ^^ { case "/" ~ list => list mkString " " }
 
   // Base parser for attributes: User-defined key-value pairings separated by ':'
-  def attribute:Parser[Tuple2[String,String]] = value ~ ":" ~ value ^^ { 
+  def attribute:Parser[(String,String)] = value ~ ":" ~ value ^^ { 
     case k~":"~v => (k -> v)
   }
   
@@ -45,24 +45,23 @@ class GrammarParser extends RegexParsers {
    * Now we build the compound parsers which will produce our grammar objects
    */
   
-  def word:Parser[Expression] = prefixes.* ~ term ~ postfixes.* ^^ {
-    case plist1~term~plist2 => Term(term, (plist1.flatten union plist2.flatten):_*)
+  def word:Parser[Yoruba] = prefixes.* ~ term ~ postfixes.* ^^ {
+    case plist1~term~plist2 => Term(term, (plist1 union plist2).flatten)
   }
   
-  def decomposition:Parser[List[Expression]] = "[" ~repsep(word, ".")~ "]" ^^ {
+  def decomposition:Parser[List[Yoruba]] = "[" ~repsep(word, ".")~ "]" ^^ {
     case "[" ~ list ~ "]" => {list}
   }
   
   def glossary:Parser[List[Meaning]] = rep(sense) ^^ { _ map (Translation(_)) }
   
-  def attribs:Parser[List[Tuple2[String,String]]] = "<"~ repsep(attribute, "|") ~">" ^^ {
+  def attribs:Parser[List[(String,String)]] = "<"~ repsep(attribute, "|") ~">" ^^ {
     case "<" ~ list ~ ">" => { list }    
   }
 
   def wordEntry:Parser[(WordEntry, List[Meaning])] = term ~ decomposition ~ glossary ~ attribs.? ^^ {
     case term ~ dcomp ~ gloss ~ attrs => {
-      var entry = WordEntry(Word(term, dcomp))
-      for (a <- attrs) { a map (entry.addAttributes(_)) }
+      var entry = WordEntry(Word(term, dcomp), attrs.getOrElse(List()).toMap)
       ((entry -> gloss))
     }
   }
@@ -70,19 +69,14 @@ class GrammarParser extends RegexParsers {
 
 object ParserTest extends GrammarParser {
   def main(args:Array[String]) {
-    var dict = YorubaDictionary(Map[WordEntry, List[Meaning]]())
-    val testEntry1 = "igba [ìgbà*]  /time"
-    val testEntry2 = "nigba [ní . <-ìgbà*]  /when"
-    val testEntry3 = "kuule [kú+>* . <-ilé]  /greetings"
-    val testEntry4 = "ade [à . dé*]  /crown"
-    val testEntry5 = "a [awa-->*]  /we"
-    
-    dict += parseAll(wordEntry, testEntry1).get
-    dict += parseAll(wordEntry, testEntry2).get
-    dict += parseAll(wordEntry, testEntry3).get
-    dict += parseAll(wordEntry, testEntry4).get
-    dict += parseAll(wordEntry, testEntry5).get
-    
-     for (entry <- dict) println(entry._1.word, entry._2)
+    val test = List("igba [ìgbà*]  /time",
+    				"nigba [ní . <-ìgbà*]  /when",
+    				"kuule [kú+> . <-ilé]  /greetings",
+    				"ade [à . dé*]  /crown",
+    				"a [awa-->*]  /we"
+    			   )
+    val inputs = for (entry <- test) yield parse(wordEntry, entry).get
+    val dict = YorubaDictionary(inputs.toMap)
+    for (entry <- dict) println(entry)
   }
 }
