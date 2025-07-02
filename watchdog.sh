2c1b039bf477c01c5a6abc113c4cf92f9ed4d4da
Damola Mabogunje <damola@mabogunje.net>
2025-07-02 14:32:05 +0100

    [Bugfix]: Updated watchdog script to correctly watch for sbt and wait for 1 minute between checks

--- watchdog.sh
+++ watchdog.sh
@@ -2,10 +2,10 @@
 
 while true; do
     # Check if the sbt process is running
-    if ! pgrep -f "sbt run" > /dev/null; then
+    if ! pgrep -f "sbt" > /dev/null; then
         echo "sbt process not found. Restarting..."
         sbt run &
     fi
-    # Wait for 5 seconds before checking again
-    sleep 5
+    # Wait for 60 seconds before checking again
+    sleep 60
 done
